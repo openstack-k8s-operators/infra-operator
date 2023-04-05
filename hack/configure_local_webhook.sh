1566e6a229c848afc51549bce776d7505f241c15
Andrew Bays <abays@redhat.com>
2023-04-05 16:33:26 +0000

    Webhooks for default images

---
+++ hack/configure_local_webhook.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+set -ex
+
+TMPDIR=${TMPDIR:-"/tmp/k8s-webhook-server/serving-certs"}
+SKIP_CERT=${SKIP_CERT:-false}
+CRC_IP=${CRC_IP:-$(/sbin/ip -o -4 addr list crc | awk '{print $4}' | cut -d/ -f1)}
+FIREWALL_ZONE=${FIREWALL_ZONE:-"libvirt"}
+
+#Open 9443
+sudo firewall-cmd --zone=${FIREWALL_ZONE} --add-port=9443/tcp
+sudo firewall-cmd --runtime-to-permanent
+
+# Generate the certs and the ca bundle
+if [ "$SKIP_CERT" = false ] ; then
+    mkdir -p ${TMPDIR}
+    rm -rf ${TMPDIR}/* || true
+
+    openssl req -newkey rsa:2048 -days 3650 -nodes -x509 \
+    -subj "/CN=${HOSTNAME}" \
+    -addext "subjectAltName = IP:${CRC_IP}" \
+    -keyout ${TMPDIR}/tls.key \
+    -out ${TMPDIR}/tls.crt
+
+    cat ${TMPDIR}/tls.crt ${TMPDIR}/tls.key | base64 -w 0 > ${TMPDIR}/bundle.pem
+
+fi
+
+CA_BUNDLE=`cat ${TMPDIR}/bundle.pem`
+
+# Patch the webhook(s)
+cat >> ${TMPDIR}/patch_webhook_configurations.yaml <<EOF_CAT
+apiVersion: admissionregistration.k8s.io/v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: vopenstackclient.kb.io
+webhooks:
+- admissionReviewVersions:
+  - v1
+  clientConfig:
+    caBundle: ${CA_BUNDLE}
+    url: https://${CRC_IP}:9443/validate-client-openstack-org-v1beta1-openstackclient
+  failurePolicy: Fail
+  matchPolicy: Equivalent
+  name: vopenstackclient.kb.io
+  objectSelector: {}
+  rules:
+  - apiGroups:
+    - client.openstack.org
+    apiVersions:
+    - v1beta1
+    operations:
+    - CREATE
+    - UPDATE
+    resources:
+    - openstackclients
+    scope: '*'
+  sideEffects: None
+  timeoutSeconds: 10
+---
+apiVersion: admissionregistration.k8s.io/v1
+kind: MutatingWebhookConfiguration
+metadata:
+  name: mopenstackclient.kb.io
+webhooks:
+- admissionReviewVersions:
+  - v1
+  clientConfig:
+    caBundle: ${CA_BUNDLE}
+    url: https://${CRC_IP}:9443/mutate-client-openstack-org-v1beta1-openstackclient
+  failurePolicy: Fail
+  matchPolicy: Equivalent
+  name: mopenstackclient.kb.io
+  objectSelector: {}
+  rules:
+  - apiGroups:
+    - client.openstack.org
+    apiVersions:
+    - v1beta1
+    operations:
+    - CREATE
+    - UPDATE
+    resources:
+    - openstackclients
+    scope: '*'
+  sideEffects: None
+  timeoutSeconds: 10
+---
+apiVersion: admissionregistration.k8s.io/v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: vmemcached.kb.io
+webhooks:
+- admissionReviewVersions:
+  - v1
+  clientConfig:
+    caBundle: ${CA_BUNDLE}
+    url: https://${CRC_IP}:9443/validate-memcached-openstack-org-v1beta1-memcached
+  failurePolicy: Fail
+  matchPolicy: Equivalent
+  name: vmemcached.kb.io
+  objectSelector: {}
+  rules:
+  - apiGroups:
+    - memcached.openstack.org
+    apiVersions:
+    - v1beta1
+    operations:
+    - CREATE
+    - UPDATE
+    resources:
+    - memcacheds
+    scope: '*'
+  sideEffects: None
+  timeoutSeconds: 10
+---
+apiVersion: admissionregistration.k8s.io/v1
+kind: MutatingWebhookConfiguration
+metadata:
+  name: mmemcached.kb.io
+webhooks:
+- admissionReviewVersions:
+  - v1
+  clientConfig:
+    caBundle: ${CA_BUNDLE}
+    url: https://${CRC_IP}:9443/mutate-memcached-openstack-org-v1beta1-memcached
+  failurePolicy: Fail
+  matchPolicy: Equivalent
+  name: mmemcached.kb.io
+  objectSelector: {}
+  rules:
+  - apiGroups:
+    - memcached.openstack.org
+    apiVersions:
+    - v1beta1
+    operations:
+    - CREATE
+    - UPDATE
+    resources:
+    - memcacheds
+    scope: '*'
+  sideEffects: None
+  timeoutSeconds: 10
+EOF_CAT
+
+oc apply -n openstack -f ${TMPDIR}/patch_webhook_configurations.yaml
