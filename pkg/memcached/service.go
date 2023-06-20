59bcd1cab58e0915468a99e01f2734e4e01dc450
Takashi Kajinami <tkajinam@redhat.com>
2023-06-20 11:57:16 +0900

    Remove 'memcached' from resource names
    Since [1] was merged now memcached CR is created with the names which
    contain 'memcached' as their suffixes, so adding additional memcached
    causes redundant names (eg memcached-horizon-memcached).
    
    Because our operators does not add CR name to resources in general,
    this stops adding memcached to resource names and makes the resource
    name more simple.
    
    [1] https://github.com/openstack-k8s-operators/horizon-operator/pull/138

--- pkg/memcached/service.go
+++ pkg/memcached/service.go
@@ -1,8 +1,6 @@
 package memcached
 
 import (
-	"fmt"
-
 	memcachedv1 "github.com/openstack-k8s-operators/infra-operator/apis/memcached/v1beta1"
 	labels "github.com/openstack-k8s-operators/lib-common/modules/common/labels"
 	service "github.com/openstack-k8s-operators/lib-common/modules/common/service"
@@ -14,14 +12,14 @@ func HeadlessService(m *memcachedv1.Memcached) *corev1.Service {
 	labels := labels.GetLabels(m, "memcached", map[string]string{
 		"owner": "infra-operator",
 		"cr":    m.GetName(),
-		"app":   fmt.Sprintf("memcached-%s", m.GetName()),
+		"app":   m.GetName(),
 	})
 	details := &service.GenericServiceDetails{
 		Name:      m.GetName(),
 		Namespace: m.GetNamespace(),
 		Labels:    labels,
 		Selector: map[string]string{
-			"app": fmt.Sprintf("memcached-%s", m.GetName()),
+			"app": m.GetName(),
 		},
 		Port: service.GenericServicePort{
 			Name:     "memcached",
