1d1beaeaab6b416f3be836ac79c6f6173641f000
Takashi Kajinami <tkajinam@redhat.com>
2023-07-14 15:46:43 +0900

    Ensure webhook is started
    This ensures webhook is started when controller-manage deployment
    becomes ready.

--- main.go
+++ main.go
@@ -190,6 +190,7 @@ func main() {
 	networkv1.SetupDefaults()
 
 	// Setup webhooks if requested
+	checker := healthz.Ping
 	if strings.ToLower(os.Getenv("ENABLE_WEBHOOKS")) != "false" {
 		if err = (&memcachedv1.Memcached{}).SetupWebhookWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "Memcached")
@@ -215,15 +216,16 @@ func main() {
 			setupLog.Error(err, "unable to create webhook", "webhook", "IPSet")
 			os.Exit(1)
 		}
+		checker = mgr.GetWebhookServer().StartedChecker()
 	}
 
 	//+kubebuilder:scaffold:builder
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+	if err := mgr.AddHealthzCheck("healthz", checker); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
 	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+	if err := mgr.AddReadyzCheck("readyz", checker); err != nil {
 		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
