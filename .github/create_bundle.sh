499a1098aa86b492a40333d9e2fa91c1546e0aca
Dan Prince <dprince@redhat.com>
2023-08-30 14:21:41 -0400

    Upgrade to operator-sdk-1.31

--- .github/create_bundle.sh
+++ .github/create_bundle.sh
@@ -9,8 +9,5 @@ echo "${BASE_IMAGE}"
 RELEASE_VERSION=$(grep "^VERSION" Makefile | awk -F'?= ' '{ print $2 }')
 echo "Release Version: $RELEASE_VERSION"
 
-#FIXME(dprince) just a test to see if this works in CI
-skopeo inspect docker://registry.redhat.io/rhel9/redis-6:latest
-
 echo "Creating bundle image..."
 USE_IMAGE_DIGESTS=true VERSION=$RELEASE_VERSION IMG=${REGISTRY}/${BASE_IMAGE}:${GITHUB_SHA} make bundle
