8e55a15bddcc869f5463c76077bb449d2858a4f6
Luca Miccini <lmiccini@redhat.com>
2024-08-12 16:55:33 +0200

    Add AntiAffinity and NodeSelector capabilities
    Related: https://issues.redhat.com/browse/OSPRH-9295

--- pkg/memcached/statefulset.go
+++ pkg/memcached/statefulset.go
@@ -2,6 +2,8 @@ package memcached
 
 import (
 	memcachedv1 "github.com/openstack-k8s-operators/infra-operator/apis/memcached/v1beta1"
+	common "github.com/openstack-k8s-operators/lib-common/modules/common"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/affinity"
 	labels "github.com/openstack-k8s-operators/lib-common/modules/common/labels"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -12,9 +14,9 @@ import (
 // StatefulSet returns a Stateful resource for the Memcached CR
 func StatefulSet(m *memcachedv1.Memcached) *appsv1.StatefulSet {
 	matchls := map[string]string{
-		"app":   m.Name,
-		"cr":    m.Name,
-		"owner": "infra-operator",
+		common.AppSelector:   m.Name,
+		"cr":                 m.Name,
+		common.OwnerSelector: "infra-operator",
 	}
 	ls := labels.GetLabels(m, "memcached", matchls)
 	runAsUser := int64(0)
@@ -90,6 +92,20 @@ func StatefulSet(m *memcachedv1.Memcached) *appsv1.StatefulSet {
 				},
 			},
 		},
+	}
+
+	// If possible two pods of the same service should not
+	// run on the same worker node. If this is not possible
+	// the get still created on the same worker node.
+	sfs.Spec.Template.Spec.Affinity = affinity.DistributePods(
+		common.AppSelector,
+		[]string{
+			m.Name,
+		},
+		corev1.LabelHostname,
+	)
+	if m.Spec.NodeSelector != nil && len(m.Spec.NodeSelector) > 0 {
+		sfs.Spec.Template.Spec.NodeSelector = m.Spec.NodeSelector
 	}
 
 	return sfs
